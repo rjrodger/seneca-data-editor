05c2332c98d216f60a96ce22ca913a925dbce69e
Richard Rodger <richard@ricebridge.com>
2013-05-28 01:33:55 +0100

    entlist cmd

--- test/data-editor.test.js
+++ test/data-editor.test.js
@@ -4,12 +4,13 @@
 
 // mocha data-editor.test.js
 
+var util = require('util')
 
 var seneca  = require('seneca')
 
 var assert  = require('chai').assert
 
-
+var gex = require('gex')
 
 
 
@@ -44,16 +45,67 @@ describe('data-editor', function() {
           si.make$('sys','entity').list$({},function(err,list){
             if( err ) return done(err);
 
-            console.dir(list)
+            assert.ok( gex('*=sys;*=entity;*=sys;*=user;*=sys;*=login;*').on(util.inspect(list)) )
 
+/*
             si.act('role:mem-store,cmd:dump',function(err,data){
               console.dir(data)
 
               done()              
             })
+*/
+            done()
           })
         })
       })
   })
 
+
+
+  it('access', function( done ){
+    seneca()
+      .use('..',{init:{sys:true}})
+      .ready( function(err,si){ 
+        assert.isNull(err)
+
+        si.act('role:data-editor, cmd:init',{entities:"-/-/a,-/-/b,-/A/t,-/A/u,-/B/v"},function(err){
+          if( err ) return done(err);
+
+          si.make$('sys','entity').list$({},function(err,list){
+            if( err ) return done(err);
+
+            assert.ok( gex('*=sys;*=entity;*=sys;*=user;*=sys;*=login;*=a;*=b;*').on(util.inspect(list)) )
+
+
+            var user = {access:{name:'a'}}
+
+            si.act('role:data-editor,cmd:entlist',{user:user},function(err,list){
+              if( err ) return done(err);
+
+              console.dir(list)
+
+
+              user = {access:{base:'A'}}
+              
+              si.act('role:data-editor,cmd:entlist',{user:user},function(err,list){
+                if( err ) return done(err);
+
+                console.dir(list)
+                
+
+                user = {access:{base:'B',list:[{base:'sys',name:'user'},{name:'a'}]}}
+              
+                si.act('role:data-editor,cmd:entlist',{user:user},function(err,list){
+                  if( err ) return done(err);
+
+                  console.dir(list)
+
+                  done()
+                })
+              })
+            })
+          })
+        })
+      })
+  })
 })
