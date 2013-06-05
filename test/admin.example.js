1a26b9a8e350027a009d0a205a1a52e2a3cfe16c
Richard Rodger <richard@ricebridge.com>
2013-06-06 00:30:18 +0100

    entity view from toolbar

--- test/admin.example.js
+++ test/admin.example.js
@@ -5,6 +5,7 @@
 var express = require('express')
 
 var seneca  = require('seneca')()
+seneca.use('jsonrest-api')
 seneca.use('..')
 
 
@@ -14,3 +15,13 @@ app.use( seneca.service() )
 
 
 app.listen( 3000 )
+
+
+var z1_b1_n1 = seneca.make$('z1','b1','n1')
+z1_b1_n1.data$({a:1}).save$()
+z1_b1_n1.data$({a:2}).save$()
+
+var z1_b2_n1 = seneca.make$('z1','b2','n1')
+z1_b2_n1.data$({b:1}).save$()
+z1_b2_n1.data$({b:2}).save$()
+
--- web/js/data-editor-client.js
+++ web/js/data-editor-client.js
@@ -7,18 +7,84 @@
 
   var de = Seneca.data_editor = angular.module('data_editor',['ngGrid'])
 
-  de.controller('Entity', function($scope, $http) {
+  de.factory('pubsub', function() {
+    var cache = {};
+    return {
+      publish: function(topic, args) { 
+	cache[topic] && $.each(cache[topic], function() {
+	  this.apply(null, args || []);
+	});
+      },
+      
+      subscribe: function(topic, callback) {
+	if(!cache[topic]) {
+	  cache[topic] = [];
+	}
+	cache[topic].push(callback);
+	return [topic, callback]; 
+      },
+      
+      unsubscribe: function(handle) {
+	var t = handle[0];
+	cache[t] && d.each(cache[t], function(idx){
+	  if(this == handle[1]){
+	    cache[t].splice(idx, 1);
+	  }
+	});
+      }
+    }
+  });
 
-    $scope.entlist = []
 
-    $scope.gridOptions = { data: 'entlist' }
-
-    $http({method: 'GET', url: '/data-editor/entlist', cache: false}).
-      success(function(data, status) {
-        $scope.entlist = data.entlist
-        $scope.gridOptions = { data: 'entlist' }
-      })
+  de.controller('ToolBar', function($scope, pubsub) {
+    $scope.showEnts = function(){
+      pubsub.publish('view',['ents'])
+    }
   })
+
+
+  de.controller('Entity', function($scope, $http, pubsub) {
+
+    $scope.data = []
+
+    $scope.gridOptions = { 
+      data: 'data',
+      columnDefs: 'coldefs',
+      beforeSelectionChange: function(item) {
+        console.log(item.rowIndex)
+        $scope.coldefs = [
+          {field:'a'},
+        ]
+        $scope.data = [{a:1},{a:2}]
+      }
+    }
+
+    $scope.showEnts = function() {
+      $scope.coldefs = [
+        {field:'zone',},
+        {field:'base'},
+        {field:'name'}
+      ]
+      $scope.data = $scope.entlist
+    }
+
+    $scope.loadEnts = function() {
+      $http({method: 'GET', url: '/data-editor/entlist', cache: false}).
+        success(function(data, status) {
+          $scope.entlist = data.entlist
+          $scope.showEnts()
+        })
+    }
+
+    pubsub.subscribe('view',function(view){
+      if( 'ents' == view ) $scope.showEnts();
+    })
+
+
+    $scope.loadEnts()
+  })
+
+
 
 })();
 
