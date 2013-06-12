40e8616eb8165d60553dfc678df848d1ee5d8732
Richard Rodger <richard@ricebridge.com>
2013-06-12 18:19:24 +0100

    save,new

--- test/admin.example.js
+++ test/admin.example.js
@@ -9,6 +9,7 @@ seneca.use('..')
 
 
 var app = express()
+app.use( express.json() )
 
 app.use( seneca.service() )
 
@@ -17,10 +18,10 @@ app.listen( 3000 )
 
 
 var z1_b1_n1 = seneca.make$('z1','b1','n1')
-z1_b1_n1.data$({a:1}).save$()
-z1_b1_n1.data$({a:2}).save$()
+z1_b1_n1.data$({a:1,aa:1}).save$()
+z1_b1_n1.data$({a:2,aa:2}).save$()
 
 var z1_b2_n1 = seneca.make$('z1','b2','n1')
-z1_b2_n1.data$({b:1}).save$()
-z1_b2_n1.data$({b:2}).save$()
+z1_b2_n1.data$({b:1,bb:1}).save$()
+z1_b2_n1.data$({b:2,bb:2}).save$()
 
--- web/js/data-editor-client.js
+++ web/js/data-editor-client.js
@@ -80,10 +80,21 @@
       }
     }
 
+    var headers = {
+      //'Content-type': 'application/json'
+    }
+
     return {
       list: function(kind,query,done){
         var f = listfunc[kind] ? listfunc[kind] : listfunc['*'] 
         f(kind,query,done)
+      },
+      save: function(kind,item,done){
+        var idpart = void 0 != item.id ? '/'+item.id : ''
+        $http({method: 'POST', url: '/data-editor/rest/'+kind+idpart, data:item, headers:headers, cache: false}).
+          success(function(data, status) {
+            done(null,data)
+          })
       }
     }
   })
@@ -105,28 +116,34 @@
       columnDefs: 'coldefs',
       enableHighlighting: true,
       beforeSelectionChange: function(row) {
+        if($scope.opening) { 
+          $scope.opening = false
+          return
+        }
         var item = $scope.data[row.rowIndex]
         pubsub.publish('view',['detail',$scope.kind,item])
       }
     }
 
     $scope.list = function(kind) {
-      entity.list(kind,{},function(err,res){
-        $scope.kind = kind
+      $scope.kind = kind || $scope.kind
+      entity.list($scope.kind,{},function(err,res){
         $scope.coldefs = res.fields
         $scope.data = res.list
       })
     }
 
     $scope.onAction = function(name,row) {
-      console.log('onA',name,row)
+      $scope.opening = true
       var item = $scope.data[row.rowIndex]
       $scope.list([item.zone,item.base,item.name].join('_'))
     }
 
     pubsub.subscribe('view',function(view){
-      if( 'ents' == view ) $scope.list('sys_entity');
+      if( 'ents' == view ) return $scope.list('sys_entity');
+      if( 'list' == view ) return $scope.list();
     })
+
 
     $scope.list('sys_entity')
   })
@@ -137,13 +154,15 @@
 
     $scope.visible = false
     $scope.kind = 'none'
-
+    $scope.msg=''
+    
     $scope.fields = []
 
     $scope.show = function(kind,item) {
       $scope.visible = true
       $scope.kind = kind
       $scope.item = item
+      $scope.changes = _.clone(item)
       $scope.ident = item.id
 
       $scope.editable = 'sys_entity' != kind
@@ -158,12 +177,25 @@
       $scope.fields = fields
     }
 
+
     $scope.newItem = function() {
-      console.log('newItem')
+      $scope.item = {}
+      $scope.changes = {}
+      $scope.ident = undefined
     }
 
     $scope.saveItem = function() {
-      console.log('newItem')
+      if( $scope.saving ) return;
+      $scope.saving = true
+      _.each($scope.changes,function(v,k){
+        $scope.item[k]=v
+      })
+      entity.save($scope.kind,$scope.item,function(err,res){
+        $scope.ident = res.id
+        $scope.msg = 'Saved'
+        $scope.saving = false
+        pubsub.publish('view',['list'])
+      })
     }
 
     $scope.closeItem = function() {
@@ -177,6 +209,8 @@
 
   })
 
+  de.controller('Fields', function($scope, pubsub, entity) {
+  })
 
 })();
 
